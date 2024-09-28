9a479618d466ab660d9f2e54d18c573c81c9e6b7
Vitor Xavier <vitorxavier012@gmail.com>
2024-09-27 23:14:55 -0300

    Mudanças

--- conta-banco/conta-banco/src/ContaTerminal.java
+++ conta-banco/conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o numero da conta: ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Digite o numero da agencia: ");
+        String agencia = scanner.next();
+
+        System.out.println("Insira nome do cliente: ");
+        String cliente = scanner.next();
+
+        System.out.println("O seu saldo e de: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Ola " + cliente + " Obrigado por criar uma conta em nosso banco,sua agencia é "+ agencia + " conta "+ conta + " e seu saldo "+ saldo + " ja esta disponivel para saque.");
     }
 }
